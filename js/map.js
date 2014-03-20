7d4647c73d1af8062e1ccbc46440e425ad3b3569
Matthew B. Gray <himself@matthew.geek.nz>
2014-03-20 20:32:51 +1300

    Pear programming with Seno: Refactored bubble template out into handlebars

--- js/map.js
+++ js/map.js
@@ -1,14 +1,22 @@
 (function($) {
-  var centerMap = { 'lng': 174.77418326567965, 'lat': -41.26205894817599 };
-  // create a map in the "map" div, set the view to a given place and zoom
-
+  // Setup the map
+  var centerMap = {
+    'lng': 174.77418326567965,
+    'lat': -41.26205894817599
+  };
   var map = L.map('map').setView(centerMap, 13);
+
   // add an OpenStreetMap tile layer
   L.tileLayer( 'http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       'attribution': '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
   $.getJSON('wellington.json', function(sampleData){
+    // Compile the bubble's template ready for rendering in the map
+    var bubbleTemplate = Handlebars.compile(
+      $('#bubble-template').text()
+    );
+
     var userLocation = null;
     var markers = [];
     var locationData = sampleData.vectorQuery.layers['2219'];
@@ -25,32 +33,36 @@
     $.each( locationData.features, function dropMarker(i, datum) {
       if ( datum && datum.geometry && datum.geometry.coordinates ) {
         var seed   = new Date().getTime()
-      var rating = Math.floor( Math.random( seed ) * 5 ) + 1; // 1 to 5
-    var marker = L.marker(datum.geometry.coordinates).addTo(map);
-    markers.push( marker );
+        var rating = Math.floor( Math.random( seed ) * 5 ) + 1; // 1 to 5
+        var marker = L.marker(datum.geometry.coordinates).addTo(map);
+        markers.push( marker );
 
-    var stars;
-    switch(rating) {
-      case 1 : stars = 'S <span class="grey">S S S S</span>';     break;
-      case 2 : stars = 'S S <span class="grey">S S S</span>';    break;
-      case 3 : stars = 'S S S <span class="grey">S S</span>';   break;
-      case 4 : stars = 'S S S S <span class="grey">S</span>';  break;
-      case 5 : stars = 'S S S S S'; break;
-    }
+        var yellowStars;
+        var greyStars;
+        switch(rating) {
+          case 1 : yellowStars = 'S'; greyStars = 'S S S S';     break;
+          case 2 : yellowStars = 'S S'; greyStars = 'S S S';    break;
+          case 3 : yellowStars = 'S S S'; greyStars = 'S S';   break;
+          case 4 : yellowStars = 'S S S S'; greyStars = 'S';  break;
+          case 5 : yellowStars = 'S S S S S'; greyStars = ''; break;
+        }
 
-    // FIXME this should be templated
-    var bubbleContent = '';
-    bubbleContent += '<h2>' + datum.properties.Location + '</h2>';
-    bubbleContent += '<p><span class="icon">' + stars + '</span></p>';
-    bubbleContent += '<p class="hours">' + datum.properties.Open_hours + '</p>';
-    if ( datum.properties.Disabled === 'Yes' ) {
-      bubbleContent += '<img src="images/disabled.png"> ' + 'Disability friendly';
-    }else {
-      bubbleContent += 'No Disability';
-      // return;
-    }
-    marker.bindPopup( bubbleContent );
-    window.loo = datum.properties;
+        // FIXME this should be templated
+        var bubbleContent = '';
+        bubbleContent += '<p class="hours">' + datum.properties.Open_hours + '</p>';
+        if ( datum.properties.Disabled === 'Yes' ) {
+          bubbleContent += '<img src="images/disabled.png"> ' + 'Disability friendly';
+        }
+        else {
+          bubbleContent += 'No Disability';
+          // return;
+        }
+
+        datum.yellowStars = yellowStars;
+        datum.greyStars = greyStars;
+        marker.bindPopup(
+          bubbleTemplate(datum)
+        );
       }
     });
 
